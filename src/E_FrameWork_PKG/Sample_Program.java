915b42394768369c24dbeb901cb272d59856f245
Ed D Singh <indrajeetsingh6493@gmail.com>
2017-06-28 11:29:01 +0530

    Updated

--- src/E_FrameWork_PKG/Sample_Program.java
+++ src/E_FrameWork_PKG/Sample_Program.java
@@ -1,36 +1,25 @@
 package E_FrameWork_PKG;
 
-import E_FrameWork_PKG.E_FrameWork;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Sample_Program {
 	public static void main(String args[]) throws Exception
 	{
-		FUNCTION_NAME1();	
-	}
-	public static void FUNCTION_NAME1() throws Exception
-	{
-		//DECLARATION SECTION
+		System.setProperty("webdriver.gecko.driver", "C:\\E_FrameWork\\Automation JARS_Drivers\\Browser Drivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://mail.google.com");
+		Thread.sleep(4000);
+		System.out.println("Badshahooo..! In da Process");
+		try{
+			driver.findElement(By.xpath("//table[@id=':ug']/tbody/tr/td[2]/div[2]/div")).sendKeys("Balle Balle");
+		}catch(org.openqa.selenium.NoSuchElementException NSE)
+		{
+			driver.findElement(By.xpath(".//*[@id=':n6']")).sendKeys("Balle Balle...................!");
 			
-			//CRAETED OBJECT OF fRAMEwORK CLASS
-				E_FrameWork driv = new E_FrameWork();
-			//CALL OBJECT FILE(REPOSITORY)
-				driv.E_CallWebObjectsFile("PHP_Travels");
-			//HTML REPORT NAME	
-				driv.EReport_CreateHTMLReport("PHP_Travel");	
-			//TEST DATA FILE NAME&PATH
-				driv.ETestData_File("C:\\E_FrameWork\\TestDataFiles\\PHP_Travels.xlsx", "Sheet1");
-
-		//CODE SECTION
-				driv.E_LoadURL("#URL");
-				driv.E_WaitForPageLoad();
-				driv.E_Click("HOTELS_TAB.XPATH");
-				driv.E_WaitUntilElementDisplay("Location_txt.ID");
-				driv.E_Write("Location_txt.ID", "#Location");
-				driv.E_Click("TOURS_TAB.XPATH");
-				driv.E_WaitUntilElementDisplay("Search_By_Listing.XPATH");
-				driv.E_Write("Search_By_Listing.XPATH", "#Tours");
-				
-		//CLOSING SECTION
-				driv.Efile_CloseALL();
+		}
 	}
+
 }
 
